fed88d3c7510d5fc8f32d71aa9bcdd856b5463bd
alvinchubusiness-boop <253842851+alvinchubusiness-boop@users.noreply.github.com>
2026-02-04 22:47:03 -0500

    Add Fibonacci Sequence mini project

---
+++ The Fibonacci Sequence/main.py
@@ -0,0 +1,15 @@
+num1 = 0
+num2 = 1
+
+count = int(input("How many Fibonacci numbers do you want? "))
+
+
+for i in range(count):
+
+    print(num1)
+    
+  
+    nth = num1 + num2
+
+    num1 = num2
+    num2 = nth
